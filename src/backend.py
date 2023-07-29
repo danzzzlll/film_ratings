9d49911c9f7bb60dc54bdebb204e5d929fd90231
danzzzlll <87424824+danzzzlll@users.noreply.github.com>
2023-07-30 01:04:02 +0300

    Update backend.py

--- src/backend.py
+++ src/backend.py
@@ -17,8 +17,8 @@ def get_prediction(text):
     return max_index
 
 def load_models(path:str):
-    model = BertForSequenceClassification.from_pretrained(path+'model', num_labels=8)
-    tokenizer = BertTokenizerFast.from_pretrained(path+'tokenizer', do_lower_case=True)
+    model = BertForSequenceClassification.from_pretrained(path+'model/', num_labels=8)
+    tokenizer = BertTokenizerFast.from_pretrained(path+'tokenizer/', do_lower_case=True)
     return model, tokenizer
 
 def sentiment(rating):
